9c124d111343e4ae60c8f892e8e42103979eb314
ICONOS\lambrm1 <marco.lambrechts@ida-mediafoundry.be>
2018-02-12 08:42:20 +0100

    no message

---
+++ src/components/Project.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Project extends Component {
+    render() {
+        return (
+            <div className="Project">
+                <p>
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Project;
